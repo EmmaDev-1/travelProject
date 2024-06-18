4119e053df68c9dc3f3a05a9421d641933bb3abb
EmmaDev1 <emma_3a@outlook.com>
2024-06-18 12:15:03 -0600

    Country, budget, summary, history and language sections creation
    Creation of "country-selection, budget-input, summary, history, language-switch" sections

---
+++ travelPageFrontEnd/src/app/country-selection/country-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-selection.component.html',
+  styleUrl: './country-selection.component.css'
+})
+export class CountrySelectionComponent {
+
+}
